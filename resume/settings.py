36dc11074180c3aab667b249aa357c20e1a262ff
Mk058 <mohankumarmk058@gmail.com>
2022-11-07 14:55:45 +0530

    deploy4

--- resume/settings.py
+++ resume/settings.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 django_heroku.settings(locals())
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

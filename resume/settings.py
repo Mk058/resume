74d0821094d5be4018e5790768ea8b1659085404
Mk058 <mohankumarmk058@gmail.com>
2022-11-09 20:31:42 +0530

    worked on navbar

--- resume/settings.py
+++ resume/settings.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 django_heroku.settings(locals())
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
